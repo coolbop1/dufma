1aef03436bf0e10d661a041b45bce840a75836cc
coolbop1 <bidoritunmise@gmail.com>
2020-02-09 15:41:57 +0100

    mobile tab

--- js/main.js
+++ js/main.js
@@ -4,19 +4,18 @@ const darkOverlay = document.querySelector('#overlay');
 darkOverlay.addEventListener("click", push);
 */
 const farmerButton = document.querySelector("#farmer-tab");
+const roundFarmButton = document.querySelector("#farmer-tabs")
 const mainContent = document.querySelector(".main-content");
 const sideContent = document.querySelector(".side-content");
 farmerButton.addEventListener("click", push);
+roundFarmButton.addEventListener("click", push)
 
-const farmersList = document.querySelector("#farmers-list");
-const groupsList = document.querySelector("#groups-list");
-const productsList = document.querySelector("#products-list");
-const farmerNames = document.getElementById("farmer-names");
-const groupsNames = document.getElementById("groups-names");
-const productsNames = document.getElementById("products-names");
-farmersList.addEventListener("click", drawone);
-groupsList.addEventListener("click", drawtwo);
-productsList.addEventListener("click", drawthree);
+const farmersList = document.querySelector("#slidelist-0");
+const groupsList = document.querySelector("#slidelist-1");
+const productsList = document.querySelector("#slidelist-2");
+farmersList.addEventListener("click", draw);
+groupsList.addEventListener("click", draw);
+productsList.addEventListener("click", draw);
 
 function push() {
   if(mainContent.classList.contains("hide-main-content")){
@@ -26,6 +25,18 @@ function push() {
     mainContent.classList.add("hide-main-content")
     sideContent.classList.add("show-side-content")
   }
+}
+function draw() {
+  console.log("hiiiii");
+  let clickedTopfull = this.id;
+  let clickedTopId = clickedTopfull.replace("slidelist-","");
+  let clickedTab = document.getElementById("side-"+clickedTopId);
+  document.querySelector(".farmers-nav-active").classList.remove("farmers-nav-active")
+  document.querySelector(".farmers-show").classList.replace("farmers-show","farmers-hide");
+  clickedTab.classList.replace("farmers-hide","farmers-show");
+  document.getElementById(this.id+"td").classList.add("farmers-nav-active")
+
+
 }
 function drawone() {
  console.log("one")
